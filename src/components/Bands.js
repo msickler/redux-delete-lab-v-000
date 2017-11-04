07fd721301a0a180827b371eafbcdb557736cb81
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-04 00:33:38 +0000

    automatically backed up by learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 class Bands extends Component {
   render() {
+    const bands = this.props.store.getState().bands.map((band, index) => {
+      return <Todo text={band.text} key={index} id={band.id} store={this.props.store}
+    })
     return(
       <ul>
-        Bands Component
+        {bands}
       </ul>
     );
   }
